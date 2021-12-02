aeed0ff5a48b9e65e905c924274bc7b5710d6c11
103SABAH_ELİFTUGCE <103SABAH_ELFTUGCE@DESKTOP-HNE43R2>
2021-12-02 13:30:54 +0300

    Add project files.

---
+++ Il_Ilce_JsonOrnek/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace Il_Ilce_JsonOrnek
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ıLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ıLCEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ıLSorgulamaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ıLCESorgulamaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ıLToolStripMenuItem,
+            this.ıLCEToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(580, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ıLToolStripMenuItem
+            // 
+            this.ıLToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ıLSorgulamaToolStripMenuItem});
+            this.ıLToolStripMenuItem.Name = "ıLToolStripMenuItem";
+            this.ıLToolStripMenuItem.Size = new System.Drawing.Size(34, 24);
+            this.ıLToolStripMenuItem.Text = "IL";
+            // 
+            // ıLCEToolStripMenuItem
+            // 
+            this.ıLCEToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ıLCESorgulamaToolStripMenuItem});
+            this.ıLCEToolStripMenuItem.Name = "ıLCEToolStripMenuItem";
+            this.ıLCEToolStripMenuItem.Size = new System.Drawing.Size(50, 24);
+            this.ıLCEToolStripMenuItem.Text = "ILCE";
+            // 
+            // ıLSorgulamaToolStripMenuItem
+            // 
+            this.ıLSorgulamaToolStripMenuItem.Name = "ıLSorgulamaToolStripMenuItem";
+            this.ıLSorgulamaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ıLSorgulamaToolStripMenuItem.Text = "IL Sorgulama";
+            // 
+            // ıLCESorgulamaToolStripMenuItem
+            // 
+            this.ıLCESorgulamaToolStripMenuItem.Name = "ıLCESorgulamaToolStripMenuItem";
+            this.ıLCESorgulamaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ıLCESorgulamaToolStripMenuItem.Text = "ILCE Sorgulama";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 519);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ıLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ıLSorgulamaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ıLCEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ıLCESorgulamaToolStripMenuItem;
+    }
+}
+
